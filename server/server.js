8b9fe2f4e9fb148519b88550e10bf6caad7f1558
Sehoon Park <milkpatron@me.com>
2016-06-29 20:20:53 -0700

    Set up node cluster

--- server/server.js
+++ server/server.js
@@ -4,20 +4,11 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 
-// Initial Configuration
+// Initial Configuration & API Routes
 require('./config/initialize')(app);
-
-// API Routes
 require('./routes/api-routes')(app);
 
-app.get('/', (req, res) => {
-  res.send('hello, world');
-});
+app.get('/', (req, res) => res.send('hello, world'));
+app.get('/*', (req, res) => res.redirect('/'));
 
-app.get('/*', (req, res) => {
-  res.redirect('/');
-});
-
-http.listen(8002, 'localhost', () => {
-  console.log('(CORS-enabled) Listening on 8002...');
-});
+http.listen(8002, 'localhost', () => console.log('(CORS-enabled) Listening on 8002...'));
