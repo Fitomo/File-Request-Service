ed8fde9148c60654b09ffb9ab6c054e058949048
Sehoon Park <milkpatron@me.com>
2016-07-07 11:34:42 -0700

    Make changes for deployment

--- server/controllers/RequestController.js
+++ server/controllers/RequestController.js
@@ -1,7 +1,7 @@
 const AWS = require('aws-sdk');
-const configPath = require('path').join(__dirname, '../config/aws-config.json');
-AWS.config.loadFromPath(configPath);
-// AWS.config.update({region: 'us-west-2'});
+// const configPath = require('path').join(__dirname, '../config/aws-config.json');
+// AWS.config.loadFromPath(configPath);
+AWS.config.update({region: 'us-west-2'});
 
 const s3 = new AWS.S3();
 const fs = require('fs');
